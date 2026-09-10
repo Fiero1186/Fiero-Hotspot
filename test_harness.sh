94893289f28a1a62882ea848cc58e3f0afec50fc
Fiero <vegnesh545@gmail.com>
2026-09-10 15:43:58 +0530

    chore: add GPL-3 headers, template udev rule, and sandbox service

--- test_harness.sh
+++ test_harness.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+#
+# Fiero Hotspot - Automated Wi-Fi repeater daemon for Linux
+# Copyright (C) 2026 Fiero
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+
 # ==============================================================================
 # Fiero Hotspot - Automated Lifecycle & Resource Benchmark Test Harness
 # ==============================================================================
